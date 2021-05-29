b3c8642c6651338cd75f267052dcaaae8edae8f3
Vasiliy <60054664+zhmaylovv@users.noreply.github.com>
2021-05-29 23:06:02 +0700

    База данных должна работать норм

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
+import MySQLdb
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get ( 'SECRET_KEY' ) or 'slonik'
-    SQLALCHEMY_DATABASE_URI = "mysql://u1355337_admin:vA6tF4oX1neX5g@localhost:3306/u1355337_bets"
+    SQLALCHEMY_DATABASE_URI = "mysql://u1355337_admin:vA6tF4oX1neX5g@31.31.196.189:3306/u1355337_bets"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
