f8dc256ad1f20567ac8d6bdde427328700d861d1
Vasiliy <60054664+zhmaylovv@users.noreply.github.com>
2021-06-02 23:23:26 +0700

    BYTEA

--- app/models.py
+++ app/models.py
@@ -14,7 +14,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     pred = db.relationship('Bets', backref='author', lazy='dynamic')
-    avatar = db.Column(db.BYTEA)
+    avatar = db.Column(db.LargeBinary)
     about_me = db.Column(db.String(140))
     fio = db.Column(db.String(140))
     score = db.Column(db.Integer)
