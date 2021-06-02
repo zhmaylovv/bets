66138036b4ba8eddcaf61b3b673f2296bd46901a
Vasiliy <60054664+zhmaylovv@users.noreply.github.com>
2021-06-02 23:56:47 +0700

    BYTEA

--- app/__init__.py
+++ app/__init__.py
@@ -12,5 +12,14 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 from app import routes, models
-
-db.create_all()
+from app.models import User
+#db.create_all()
+try:
+    user_list = User.query.order_by(User.score.desc()).all ()
+except:
+    db.create_all ()
+if len(user_list) < 1:
+    admin = User(username="admin", email="14hp@ngs.ru", fio="Администратор")
+    admin.set_password("admin")
+    db.session.add ( admin )
+    db.session.commit()
