2eb20c5c24eb58986ee72e58fd1e8d8bd5a84025
Vasiliy <60054664+zhmaylovv@users.noreply.github.com>
2021-06-08 22:31:31 +0700

    config fix static

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -128,7 +128,7 @@ function loadStyle(href, callback) {
   $(".radius-radio[data-radius='" + radius + "']").attr("checked", true);
   $("#switchDark").attr("checked", theme.indexOf("dark") > 0 ? true : false);
 
-  loadStyle("{{ url_for('static', filename='/css/') }}" + theme, onStyleComplete);
+  loadStyle("http://2021.prognosov.ru/static/css/" + theme, onStyleComplete);
   function onStyleComplete() {
     setTimeout(onStyleCompleteDelayed, 300);
   }
