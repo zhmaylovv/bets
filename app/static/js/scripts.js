c78db0a68cae5232d9a821f1944e8d97e878d52a
SpoT <zhmaylovv@gmail.com>
2022-11-14 16:13:32 +0700

    Change rpl to wc

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -128,7 +128,7 @@ function loadStyle(href, callback) {
   $(".radius-radio[data-radius='" + radius + "']").attr("checked", true);
   $("#switchDark").attr("checked", theme.indexOf("dark") > 0 ? true : false);
 
-  loadStyle("http://rpl.prognosov.ru/static/css/" + theme, onStyleComplete);
+  loadStyle("http://wc.prognosov.ru/static/css/" + theme, onStyleComplete);
   function onStyleComplete() {
     setTimeout(onStyleCompleteDelayed, 300);
   }
