c2296548bc7bf28135b90054bd1889ce500d7f6b
Vasiliy <60054664+zhmaylovv@users.noreply.github.com>
2023-03-31 20:52:29 +0700

    Fix Heroku db

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ class Config(object):
 
     SECRET_KEY = os.environ.get ( 'SECRET_KEY' ) or 'slonik'
     SQLALCHEMY_DATABASE_URI = os.environ.get ( 'DATABASE_URL' )
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 '''
     SECRET_KEY = os.environ.get ( 'SECRET_KEY' ) or 'slonik'
